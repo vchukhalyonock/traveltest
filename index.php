14f10609d2aaa820ee3dce89eac8408ce94936c8
Vladimir Chukhalyonock <vchukhalyonock@gmail.com>
2019-01-31 16:09:30 +0200

    Add CI processing

--- index.php
+++ index.php
@@ -2,4 +2,8 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-$app = new \App\App('http://robotstxt.org.ru/', __DIR__);
+if(!isset($argv[1])) {
+    echo "Usage php index.php <URL>\n";
+} else {
+    $app = new \App\App($argv[1], __DIR__);
+}
