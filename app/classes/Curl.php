25392eede8e198ce8d51486d6c5052af78dce7a7
Vladimir Chukhalyonock <vchukhalyonock@gmail.com>
2019-01-31 16:22:26 +0200

    Add options to CURL for browser emulation

--- app/classes/Curl.php
+++ app/classes/Curl.php
@@ -17,6 +17,8 @@ class Curl implements ICurl {
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_HEADER, true);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)");
 
         $body = curl_exec($ch);
         $result = curl_getinfo($ch);
